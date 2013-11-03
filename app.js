6cf0806a3251abaf3ae9a5d6c16fb2bee12f336a
Søren Louv-Jansen <sqren@tradeshift.com>
2013-11-03 14:09:11 -0800

    Added Settings page

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ app.config(function ($routeProvider) {
   $routeProvider.
     when('/business-intelligence', {templateUrl: '/components/business-intelligence/template.html', controller: 'BusinessIntelligenceController'}).
     when('/contacts', {templateUrl: '/components/contacts/template.html', controller: 'ContactsController'}).
+
+    // Settings
+    when('/settings', {templateUrl: '/components/settings/template.html', controller: 'SettingsController'}).
+
+    // Transactions
     when('/transactions', {templateUrl: '/components/transactions/overview.html', controller: 'OverviewController'}).
     when('/transactions/:company_id', {templateUrl: '/components/transactions/companyTransactions.html', controller: 'CompanyTransactionsController'}).
     when('/transactions/:company_id/:transaction_id', {templateUrl: '/components/transactions/transaction.html', controller: 'TransactionController'}).
+
+    // Login
     when('/login/:redirect', {templateUrl: '/components/login/template.html', controller: 'LoginController'}).
     otherwise({redirectTo: '/business-intelligence'});
 });
@@ -21,7 +28,7 @@ app.run( function($rootScope, $location) {
   $rootScope.$on( "$locationChangeStart", function(event, next, current) {
     // user is not logged in, and should be redirected to login page
     if ( $rootScope.loggedIn !== true && next.match('/#/login') === null) {
-      console.log("Checking login");
+      console.log("Redirecting to login page");
       var redirect = next.slice((next.indexOf('/#/') + 3));
       $location.path( "/login/" + redirect );
     }
