a9924f556732ceed00db4e4c7294432fcd58ef92
Søren Louv-Jansen <sorenlouv@gmail.com>
2013-11-02 17:58:25 -0700

    Added controllers, templates and routing

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-var app = angular.module("tradeshift-mobile", ["firebase"]);
+var app = angular.module("tradeshift-mobile", ["firebase", "ngRoute"]);
+
+// router
+app.config(function ($routeProvider) {
+  'use strict';
+  $routeProvider.
+    when('/business-intelligence', {templateUrl: '/components/business-intelligence/template.html', controller: 'BusinessIntelligenceController'}).
+    when('/contacts', {templateUrl: '/components/contacts/template.html', controller: 'ContactsController'}).
+    when('/transactions', {templateUrl: '/components/transactions/template.html', controller: 'TransactionsController'}).
+    otherwise({redirectTo: '/business-intelligence'});
+});
---
+++ components/transactions/controller.js
@@ -0,0 +1,4 @@
+app.controller('TransactionsController', ['$scope', 'angularFire', 'angularFireCollection', function ($scope, angularFire, angularFireCollection) {
+  'use strict';
+
+}]);
