04d0f59c44c96dbf9b90ff1c7226b90c9d51ee2b
Sune Theodorsen <Theodorsen@Sunes-iMac.local>
2013-11-03 12:32:50 -0800

    static content

--- components/transactions/controller.js
+++ components/transactions/controller.js
@@ -1,4 +1,20 @@
 app.controller('TransactionsController', ['$scope', 'angularFire', 'angularFireCollection', function ($scope, angularFire, angularFireCollection) {
   'use strict';
 
+  $scope.addItem = function() {
+  	$('.picker').show();
+  }
+
+  $scope.hidePicker = function() {
+  	$('.picker').hide();
+  	$('.product-picker').hide()
+  	$('.select-picker').hide();
+  }
+  $scope.addProduct = function() {
+  	$('.product-picker').show();
+  }
+  $scope.selectProduct = function() {
+  	$('.select-picker').show();
+  }
+
 }]);
