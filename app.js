3fff8ada65d679060f02762f4821fa6bdc23232a
Søren Louv-Jansen <sorenlouv@gmail.com>
2013-11-02 18:20:59 -0700

    Added login

--- app.js
+++ app.js
@@ -7,5 +7,19 @@ app.config(function ($routeProvider) {
     when('/business-intelligence', {templateUrl: '/components/business-intelligence/template.html', controller: 'BusinessIntelligenceController'}).
     when('/contacts', {templateUrl: '/components/contacts/template.html', controller: 'ContactsController'}).
     when('/transactions', {templateUrl: '/components/transactions/template.html', controller: 'TransactionsController'}).
+    when('/login', {templateUrl: '/components/login/template.html', controller: 'LoginController'}).
     otherwise({redirectTo: '/business-intelligence'});
+});
+
+// login routing
+app.run( function($rootScope, $location) {
+  'use strict';
+
+  // register listener to watch route changes
+  $rootScope.$on( "$locationChangeStart", function(event, next, current) {
+    // user is not logged in, and should be redirected to login page
+    if ( !$rootScope.loggedUser && next.match('/#/login$') === null) {
+      $location.path( "/login" );
+    }
+  });
 });
---
+++ components/login/controller.js
@@ -0,0 +1,49 @@
+app.controller('LoginController', ['$scope', 'angularFire', function ($scope, angularFire) {
+  'use strict';
+
+  // Main app
+  var appRef = new Firebase("https://tradeshift-mobile.firebaseio.com");
+
+  // users
+  var usersRef = appRef.child('users');
+  $scope.users = null;
+  angularFire(usersRef, $scope, "users");
+
+  var getOrCreateCurrentUser = function(facebookUser){
+    var currentUserRef = usersRef.child(facebookUser.id);
+    currentUserRef.once('value', function(currentUserSnapshot){
+      // Create user if not exists, or fetch from DB
+      $scope.currentUser = (currentUserSnapshot.val() === null) ? {
+        name: facebookUser.name,
+        email: facebookUser.email
+      } : currentUserSnapshot.val();
+
+      // automagic updates
+      angularFire(currentUserRef, $scope, "currentUser");
+    });
+  };
+
+  //
+  var onFacebookLogin = function(error, facebookUser) {
+    if(error) console.log("Auth error", error);
+
+    // user signed in with Facebook
+    if(facebookUser){
+      getOrCreateCurrentUser(facebookUser);
+    }
+  };
+
+  // get login status
+  var auth = new FirebaseSimpleLogin(usersRef, onFacebookLogin);
+
+
+  /************* Click events ************/
+
+  // login with facebook
+  $scope.login = function(){
+    auth.login('facebook', {
+      rememberMe: true,
+      scope: 'email,user_likes'
+    });
+  };
+}]);
