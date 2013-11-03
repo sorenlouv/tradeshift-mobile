f2d6440a6c776de26aa45978e5d0125dee50f7c1
Søren Louv-Jansen <sqren@tradeshift.com>
2013-11-03 08:07:15 -0800

    Avoid redirect on every refresh

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.config(function ($routeProvider) {
     when('/business-intelligence', {templateUrl: '/components/business-intelligence/template.html', controller: 'BusinessIntelligenceController'}).
     when('/contacts', {templateUrl: '/components/contacts/template.html', controller: 'ContactsController'}).
     when('/transactions', {templateUrl: '/components/transactions/template.html', controller: 'TransactionsController'}).
-    when('/login', {templateUrl: '/components/login/template.html', controller: 'LoginController'}).
+    when('/login/:redirect', {templateUrl: '/components/login/template.html', controller: 'LoginController'}).
     otherwise({redirectTo: '/business-intelligence'});
 });
 
@@ -18,8 +18,9 @@ app.run( function($rootScope, $location) {
   // register listener to watch route changes
   $rootScope.$on( "$locationChangeStart", function(event, next, current) {
     // user is not logged in, and should be redirected to login page
-    if ( $rootScope.loggedIn !== true && next.match('/#/login$') === null) {
-      $location.path( "/login" );
+    if ( $rootScope.loggedIn !== true && next.match('/#/login') === null) {
+      var redirect = next.slice((next.indexOf('/#/') + 3));
+      $location.path( "/login/" + redirect );
     }
   });
 });
