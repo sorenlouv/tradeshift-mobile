88a5483e515d8f5868e308300ee01319a2cd0872
Frank Jensen <me@taheilo.com>
2013-11-03 14:45:54 -0800

    deleted shit

--- components/activity/activity.js
+++ components/activity/activity.js
@@ -11,4 +11,43 @@ app.controller('ActivityController', ['$scope', '$routeParams', 'angularFire', f
 
   activity = new Firebase("https://tradeshift-mobile.firebaseio.com/transactions/" + handle);
 
+
+
+
+
+  // Sune's stuff
+  $scope.addItem = function() {
+  	$('.picker').show();
+  }
+
+  $scope.hidePicker = function() {
+  	$('.picker').hide();
+  	$('.product-picker').hide()
+  	$('.select-picker').hide();
+  }
+  $scope.addProduct = function() {
+  	$('.product-picker').show();
+  }
+  $scope.selectProduct = function() {
+  	$('.select-picker').show();
+  }
+
 }]);
+
+
+
+
+
+// app.controller('TransactionController', ['$scope', 'angularFire', '$routeParams', function ($scope, angularFire, $routeParams) {
+//   'use strict';
+
+//   var company_id      = $routeParams.company_id,
+//       transaction_id  = $routeParams.transaction_id,
+//       transaction     = new Firebase("https://tradeshift-mobile.firebaseio.com/transactions/" + transaction_id);
+  
+//   $scope.transaction = {};
+//   angularFire(transaction, $scope, 'transaction');
+
+//   $scope.transaction_id = transaction_id;
+
+// }]);
--- components/transactions/overview.js
+++
@@ -1,9 +0,0 @@
-app.controller('OverviewController', ['$scope', 'angularFire', function ($scope, angularFire) {
-  'use strict';
-
-  var companyRef = new Firebase("https://tradeshift-mobile.firebaseio.com/companies").child();
-
-  $scope.company = {};
-  angularFire(companyRef, $scope, 'company');
-
-}]);
