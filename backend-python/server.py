4dd8c816a812b1dc23151e0e1e09803d9cb7f3e2
Gabrielius Gudynas <ggu@tradeshift.com>
2013-11-03 16:51:13 -0800

    dispatch draft

--- backend-python/server.py
+++ backend-python/server.py
@@ -9,12 +9,25 @@ def index(name='World'):
 def sendDocument():
 	documentJson = request.json
 	docUUID = uuid.uuid1()
-	response = putDraft(docUUID, documentJson)
+	draftResponse = putDraft(docUUID, documentJson)
+	if draftResponse != 204:
+		return draftResponse
+	dispatchUUID = uuid.uuid1()
+	sendResponse = dispatchDraft(docUUID, dispatchUUID)
+	if sendResponse != 201:
+		return sendResponse
 
 def putDraft(docUUID, documentJson):
 	url = 'http://localhost:8888/tradeshift-backend/rest/external/documents/'+str(docUUID)+'?documentProfileId=nes.p5.invoice.ubl.2.1.dk&draft=true'
 	headers = {'X-Tradeshift-TenantId': '3fd7d621-7f89-481f-9647-b2edf2ee9a30', 'X-Tradeshift-ActorId': '5832d481-2e54-4574-9676-0f51fe6513df', 'Content-Type': 'application/json'}
 	r = requests.put(url, data=json.dumps(documentJson), headers=headers)
 	return r.status_code
+
+def dispatchDraft(docUUID, dispatchUUID):
+    url = 'http://localhost:8888/tradeshift-backend/rest/external/documents/'+str(docUUID)+'/dispatches/'+str(dispatchUUID)
+    headers = {'X-Tradeshift-TenantId': '3fd7d621-7f89-481f-9647-b2edf2ee9a30', 'X-Tradeshift-ActorId': '5832d481-2e54-4574-9676-0f51fe6513df', 'Content-Type': 'application/json'}
+    data={'ConnectionId':'94977f3a-0e97-5e76-9f99-2e7d219fce64'}
+    r = requests.put(url, data=json.dumps(data), headers=headers)
+    return r.status_code
 
 run(host='0.0.0.0', port=8081, debug=True)
